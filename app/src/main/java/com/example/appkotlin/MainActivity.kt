eebac0965ccbd979407206687f7d4f517fec53d8
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-08-29 23:09:50 -0400

    Second commit

--- app/src/main/java/com/example/appkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appkotlin/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.appkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.widget.Button
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     var tts: TextToSpeech? = null
@@ -13,13 +15,23 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
         tts = TextToSpeech(this, this)
 
+        findViewById<Button>(R.id.btnSpeak).setOnClickListener{
+            speak()
+        }
+
+    }
+
+    private fun speak(){
+        var message: String = findViewById<TextView>(R.id.etMessage).text.toString()
+        tts!!.speak(message, TextToSpeech.QUEUE_FLUSH,null, "")
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            findViewById<TextView>(R.id.TextView).text="Hola bebe"
+            findViewById<TextView>(R.id.tvStatus).text="Hola bebe"
+            tts!!.setLanguage(Locale.US)
         } else{
-            findViewById<TextView>(R.id.TextView).text="No se puede escuchar"
+            findViewById<TextView>(R.id.tvStatus).text="No se puede escuchar"
         }
     }
 }
